b2ae8cdada51da229c26a5d33e169f27232a3370
Ruchita Bavishi <ruchita.bavishi@gmail.com>
2019-07-30 15:49:35 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.rect(20, 20, 90, 90);
+ctx.stroke();
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.rect(20, 110, 90, 90);
+ctx.stroke();
